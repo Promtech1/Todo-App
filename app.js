5c66739f19fa6af5666c74eb2e5f550af146657d
Promtech <uwagboepromise5@gmail.com>
2022-06-02 17:08:04 +0100

    Went further in localStorage implementation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list")
 const filterOption = document.querySelector(".filter-todo")
 
 //Event Listners
+document.addEventListener("DOMContentLoaded", getTodos)
 todoButton.addEventListener("click", addTodo)
 todoList.addEventListener("click", deleteCheck)
 filterOption.addEventListener("click", filterTodo)
@@ -102,4 +103,40 @@ function saveLocalTodos(todo){
 
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
+} 
+
+function getTodos(){
+    //CHECKS if todo is already in local storage
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach(function(todo){
+    //Todo DIV
+    const todoDiv = document.createElement("li");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo); 
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class = 'fas fa-check'></i>";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton)
+
+    //CHECK Trash BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class = 'fas fa-trash'></i>";
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton)
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv)
+    });
 }
