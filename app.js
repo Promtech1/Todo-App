9ece261d9628c5826f6eb00ebe1fb5b2fc7b9c95
Promtech <uwagboepromise5@gmail.com>
2022-06-02 15:03:57 +0100

    Progress on todo app so far

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ filterOption.addEventListener("click", filterTodo)
 function addTodo(event){
     //Prevent form from submiting
     event.preventDefault();
-    console.log("hello")
 
     //Todo DIV
     const todoDiv = document.createElement("li");
@@ -23,6 +22,9 @@ function addTodo(event){
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+
+    //ADD TODO TO LOCALSTORAGE
+    saveLocalTodos(todoInput.value)
 
     //CHECK MARK BUTTON
     const completedButton = document.createElement("button");
@@ -61,4 +63,43 @@ function deleteCheck(e){
         const todo = item.parentElement;
         todo.classList.toggle("completed")
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+//Setting todos to local storage
+function saveLocalTodos(todo){
+    //CHECKS if todo is already in local storage
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
